8ff2be68b8503984581ced92aa4fe9276ccf72ee
Samantha Cruz <cruz.s98319@gmail.com>
2024-05-27 10:23:08 -0700

    Created a parent component to pass down as prop for TodoForm

---
+++ src/components/TodoManager.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import TodoForm from './TodoForm'
+import TaskManager from './TaskManager'
+
+function TodoManager() {
+    const [lists, setLists] = useState([]);
+
+    const assList = (list) => {
+        setLists([...lists, list]);
+    };
+
+    return(
+        <div>
+            <TodoForm addList={addList} />
+            <h2>To-Do Lists</h2>
+            <ul>
+                {lists.map((list, index) => (
+                    <li key={index}>{list.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
