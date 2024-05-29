d09dd77c8335ad8591cc7a2a189bc12b2c61244c
Samantha Cruz <cruz.s98319@gmail.com>
2024-05-28 19:50:46 -0700

    Removed the ability to add notes for new tasks

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,21 +1,18 @@
 import React, { useState } from 'react';
 
-function TaskForm({ addTask, listId }) {
+function TaskForm({ addTask }) {
     const [value, setValue] = useState('');
-    const [notes, setNotes] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (value && notes) {
-            const newTask = { task: value, notes: notes, listId: listId };
-            console.log('New Task:', newTask);  // Log the new task
+        if (value) {
+            const newTask = { task: value };
 
             // Adding the task to the list
             addTask(newTask);
 
             // Clearing the form fields
             setValue('');
-            setNotes('');
         } else {
             console.log('Incomplete task details');
         }
@@ -32,15 +29,6 @@ function TaskForm({ addTask, listId }) {
                         value={value}
                         onChange={(e) => setValue(e.target.value)}
                         placeholder="enter task"
-                    />
-                </div>
-                <div>
-                    <label>Notes</label>
-                    <input
-                        type="text"
-                        value={notes}
-                        onChange={(e) => setNotes(e.target.value)}
-                        placeholder="enter notes"
                     />
                 </div>
                 <input type="submit" value="Add Task" />
